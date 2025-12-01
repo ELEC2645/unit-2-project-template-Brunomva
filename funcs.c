394625866ec69106d660d0fc4afb116f4c45152f
Brunomva <el23ba@leeds.ac.uk>
2025-12-01 17:47:29 +0000

    project

--- funcs.c
+++ funcs.c
@@ -1,26 +1,153 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include "funcs.h"
 
+
+/*
+
+void main_menu(void) {
+    print_main_menu();
+    int input = get_user_input();
+    select_menu_item(input);
+}
+
+int get_user_input(void) {
+    int input;
+    printf("\nSelect item: ");
+    scanf("%d", &input);  // assume valid input for this activity
+    return input;
+}
+
+void select_menu_item(int input) {
+    switch (input) {
+        case 1: menu_item_1(); break;
+        case 2: menu_item_2(); break;
+        case 3: menu_item_3(); break;
+        case 4: menu_item_4(); break;
+
+
+        case 5: printf("Exiting program...\n"); exit(0);
+        default: printf("Invalid selection. Exiting...\n"); exit(1);
+    }
+}
+
+
+
+void print_main_menu(void) {
+    printf("\n----------- Main menu -----------\n");
+    printf("|\t1. Addition\t|\n");
+    printf("|\t2. Subtraction\t|\n");
+    printf("|\t3. Multiplication\n");
+    printf("|\t4. Division\t\t|\n");
+    printf("|\t5. Exit\t\t\t|\n");
+    printf("---------------------------------\n");
+}
+
+void go_back_to_main(void) {
+    char input;
+    // this do loop ensures user inputs 'b' or 'B' to go back
+    // if user inputs other characters, it keeps prompting
+    do {
+        printf("\nEnter 'b' or 'B' to go back to main menu: ");
+        scanf(" %c", &input);
+    } while (input != 'b' && input != 'B');
+    main_menu();
+}
+
+
+
+// --------------- Menu Item Functions ---------------
+
 void menu_item_1(void) {
-    printf("\n>> Menu 1\n");
-    printf("\nSome code here does something useful\n");
-    /* you can call a function from here that handles menu 1 */
+     print_menu_item_11();
+    go_back_to_main();
 }
 
 void menu_item_2(void) {
-    printf("\n>> Menu 2\n");
-    printf("\nSome code here does something useful\n");
-    /* you can call a function from here that handles menu 2 */
+    printf("\n>> Menu 2: Subtraction\n");
+     printf("Enter a number\n");
+    int num1;
+    scanf("%d", &num1);
+    printf("Enter a number\n");
+    int num2;
+    scanf("%d",&num2);
+     int result = num1 - num2;
+    printf("Result: %d\n", result);
+
+  
+    go_back_to_main();
 }
+
 
 void menu_item_3(void) {
-    printf("\n>> Menu 3\n");
-    printf("\nSome code here does something useful\n");
-    /* you can call a function from here that handles menu 3 */
+     printf("\n>> Menu 2: Multiplication\n");
+     printf("Enter a number\n");
+    int num1;
+    scanf("%d", &num1);
+    printf("Enter a number\n");
+    int num2;
+    scanf("%d",&num2);
+     int result = num1 * num2;
+    printf("Result: %d\n", result);
+    go_back_to_main();
+   
 }
 
+
 void menu_item_4(void) {
-    printf("\n>> Menu 4\n");
-    printf("\nSome code here does something useful\n");
-    /* you can call a function from here that handles menu 4 */
+    printf("\n>> Menu 2: Division\n");
+     printf("Enter a number\n");
+    int num1;
+    scanf("%d", &num1);
+    printf("Enter a number\n");
+    int num2;
+    scanf("%d",&num2);
+     int result = num1/num2;
+    printf("Result: %d\n", result);
+    go_back_to_main();
 }
+
+//menus inside the menus
+
+void print_menu_item_11(void){
+    printf("\n----------- Potential divider -----------\n");
+    printf("|\t1. Vin\t|\n");
+    printf("|\t2. Vout\t|\n");
+    printf("|\t3. R1\n");
+    printf("|\t4. R2\t\t|\n");
+    printf("|\t5. Exit\t\t\t|\n");
+    printf("---------------------------------\n");
+
+
+}
+*/
+
+//inside menu 1 potential divider
+// When Vout is the subject
+void find_Vout(void){
+    char buf[100];
+   
+    double Vin;
+    double R1;
+    double R2;
+    printf("\n Enter value for Vin");
+    if (fgets(buf, sizeof buf, stdin)) {
+        Vin  = strtod(buf, NULL);
+        printf("Vin = %f\n", Vin);
+    }
+    printf("\n Enter value for R1");
+     if (fgets(buf, sizeof buf, stdin)) {
+        R1 = strtod(buf, NULL);
+        printf("R1 = %f\n", R1);
+    }
+    printf("\n Enter value for R2"); 
+    if (fgets(buf, sizeof buf, stdin)) {
+        R2  = strtod(buf, NULL);
+        printf("R2 = %f\n", R2);
+    }
+
+    double Vout = Vin * (R2/R2+R1);
+
+    printf("\n Vout= %f\n",Vout);
+}
+
--- funcs.h
+++ funcs.h
@@ -1,9 +1,19 @@
 #ifndef FUNCS_H
 #define FUNCS_H
 
-void menu_item_1(void);
-void menu_item_2(void);
-void menu_item_3(void);
-void menu_item_4(void);
+void main_menu(void);
+int  get_user_input(void);
+void select_menu_item(int input);
+void print_main_menu(void);
+void go_back_to_main(void);
+
+
+void menu_item_1(void); 
+void menu_item_2(void); 
+void menu_item_3(void); 
+void menu_item_4(void); 
+void menu_item_5(void);
+
+void find_Vout(void);
 
 #endif
